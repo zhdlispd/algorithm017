c837c401977189c18d993608b78f70b18cdf0427
Chen Li <chenli@Chens-MacBook-Pro.local>
2020-10-05 00:43:36 -0400

    second week hw

---
+++ Week_02/n-ary-tree-level-order-traversal.java
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+         List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node curr = queue.poll();
+                level.add(curr.val);
+                List<Node> children = curr.children;
+                for (Node child : children) {
+                    if (child != null) {
+                        queue.add(child);
+                    }
+                }
+            }
+            result.add(level);
+        }
+
+        return result;
+    }
+
+
+    
+}
